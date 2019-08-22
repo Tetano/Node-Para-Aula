49e4defd0ec30dee8013b0c0c2d8a0b1a8263335
Tetano <viniciusvicco@gmail.com>
2019-08-22 15:14:41 -0300

    correcao aspas simples

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-    res.send(<html><body><h4>INDEX</h4></body></html>)
+    res.send("<html><body><h4>INDEX</h4></body></html>")
 });
 
 app.get('/nativas',function(req,res){
-    res.send(<html><body><h4>projeto</h4></body></html>)
+    res.send("<html><body><h4>Pessoas</h4></body></html>")
 });
 
 app.get('/arvores',function(req,res){
-    res.send(<html><body><h4>Pequi,Ipê</h4></body></html>)
+    res.send("<html><body><h4>Pequi,Cajuí</h4></body></html>")
 });
 
 app.listen(3000, function(){
