41c3ef6464e8f2a56db1d2d52bb90eb81f7a3a09
Tetano <viniciusvicco@gmail.com>
2019-08-22 15:02:26 -0300

    modo pratico de npm req,res

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+
+app.get('/',function(req,res){
+    res.send(<html><body><h4>INDEX</h4></body></html>)
+});
+
+app.get('/nativas',function(req,res){
+    res.send(<html><body><h4>projeto</h4></body></html>)
+});
+
+app.get('/arvores',function(req,res){
+    res.send(<html><body><h4>Pequi,Ipê</h4></body></html>)
+});
+
 app.listen(3000, function(){
     console.log("Servidor rodando com Express")
 });
