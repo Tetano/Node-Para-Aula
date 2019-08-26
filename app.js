09d7dac503bdd6555c9b30f4607ce3538ba5086a
Tetano <viniciusvicco@gmail.com>
2019-08-26 15:38:40 -0300

    Linkando CSS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
 app.get('/',function(req,res){
     res.render("home/index.ejs")
@@ -12,5 +13,5 @@ app.get('/noticias',function(req,res){
     res.render("noticias/noticias")
 });
 app.listen(3000, function(){
-    console.log("Servidor rodando com Express")
+    console.log("Teste ")
 });
